2cbfc5f6a48a5d689126910586a4e1d9d56ce629
KermX <KermitVIII@gmail.com>
2023-10-05 05:09:42 -0500

    Initial commit

---
+++ src/main/java/me/kermx/incomehistory/IncomeHistory.java
@@ -0,0 +1,17 @@
+package me.kermx.incomehistory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IncomeHistory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
