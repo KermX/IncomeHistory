2599b6cefa90848b5eb14c3e9887635e1a2f35ca
KermX <KermitVIII@gmail.com>
2023-10-05 19:59:42 -0500

    command changes

--- src/main/java/me/kermx/incomehistory/PaymentHistoryCommand.java
+++ src/main/java/me/kermx/incomehistory/PaymentHistoryCommand.java
@@ -17,15 +17,30 @@ public class PaymentHistoryCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String s, String[] strings) {
-        if (!(sender instanceof Player)){
-            sender.sendMessage("This command can only be used by players!");
-            return true;
+        // Check if a username argument is provided
+        if (strings.length > 0) {
+            String targetUsername = strings[0];
+            Player targetPlayer = plugin.getServer().getPlayer(targetUsername);
+
+            // Check if the provided username corresponds to an online player
+            if (targetPlayer != null) {
+                UUID targetPlayerID = targetPlayer.getUniqueId();
+                double paymentAmount = plugin.getPlayerPaymentAmount(targetPlayerID);
+                String roundedPaymentAmount = decimalFormat.format(paymentAmount);
+                sender.sendMessage("Payment info for " + targetPlayer.getName() + " over the last hour: " + roundedPaymentAmount);
+            } else {
+                sender.sendMessage("Player '" + targetUsername + "' is not online.");
+            }
+        } else if (sender instanceof Player) {
+            // No argument provided, display the sender's payment history
+            UUID playerID = ((Player) sender).getUniqueId();
+            double paymentAmount = plugin.getPlayerPaymentAmount(playerID);
+            String roundedPaymentAmount = decimalFormat.format(paymentAmount);
+            sender.sendMessage("Your payment over the last hour: " + roundedPaymentAmount);
+        } else {
+            sender.sendMessage("Usage: /paymentinfo <username>");
         }
-        Player player = (Player) sender;
-        UUID playerID = player.getUniqueId();
-        double paymentAmount = plugin.getPlayerPaymentAmount(playerID);
-        String roundedPaymentAmount = decimalFormat.format(paymentAmount);
-        player.sendMessage("Your payment over the last hour: " + roundedPaymentAmount);
+
         return true;
     }
 }
